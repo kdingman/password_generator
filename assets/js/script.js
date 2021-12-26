5ffd5f0508a08891c07b17cb7585f3b2d4e232fc
Kelly Dingman <93087116+kdingman@users.noreply.github.com>
2021-12-26 17:37:49 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,74 +5,81 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "{", "}", "[", "]", "|", "`", "?", ">", "<", ";", ":", ".", ",", ];
-var passwordLength = " <= 8 || >=128 ";
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "{", "}", "[", "]", "|", "`", "?", ">", "<", ";", ":", ".", ",",];
+
+// variables defined
+var confirmLowerCase = lowerCase;
+var confirmUpperCase = upperCase;
+var confirmNumericValue = numericValue;
+var confirmSpecialChars = specialChars;
+var passwordLength = "";
 
 // confirm how many characters user would like to use
-function questions() {
+function askQuestions() {
+// create parameters for password
   var passwordLength = (window.prompt("How many characters would you like your password to contain?"));
-  // loop if not enough characters selected 
-  if(passwordLength < 8 || passwordLength > 128) {
-  window.alert("Password must be between 8-128 characters, Try Again!");
-  var passwordLength = (window.prompt("How many characters would you like your password to contain?"));
+      // loop if not enough characters selected 
+      if(passwordLength < 8 || passwordLength > 128) {
+        window.alert("Password must be between 8-128 characters, Try Again!");
+    var passwordLength = (window.prompt("How many characters would you like your password to contain?"));
+      }
+
+  var confirmLowerCase = confirm("Click ok to include lowercase characters");
+  var confirmUpperCase = confirm("Click ok to include uppercase characters");  
+  var confirmNumericValue = confirm("Click ok to include numeric characters");    
+  var confirmSpecialChars = confirm("Click ok to include special characters");
+  var responses = {
+    confirmLowerCase: confirmLowerCase,
+    confirmUpperCase: confirmUpperCase,
+    confirmNumericValue: confirmNumericValue,
+    confirmSpecialChars: confirmSpecialChars,
+    passwordLength: passwordLength
+   }
+
+    // loop if outside of parameter
+    if(confirmLowerCase === false && confirmUpperCase === false && confirmNumericValue === false && confirmSpecialChars === false) {
+      window.alert("You must select at least one character type, Try Again.")
+  var confirmLowerCase = confirm("Click ok to include lowercase characters");   
+  var confirmUpperCase = confirm("Click ok to include uppercase characters");    
+  var confirmNumericValue = confirm("Click ok to include numeric characters");   
+  var confirmSpecialChars = confirm("Click ok to include special characters");
+    }
+    return responses;
   }
 
-// create parameters for password
-  var lowerCase = confirm("Click ok to include lowercase characters");
-  var upperCase = confirm("Click ok to include uppercase characters");  
-  var numericValue = confirm("Click ok to include numeric characters");    
-  var specialChars = confirm("Click ok to include special characters");
- 
-  // loop if outside of parameter
-  if(lowerCase === false && upperCase === false && numericValue === false && specialChars === false) {
-    window.alert("You must select at least one character type, Try Again.")
-  var lowerCase = confirm("Click ok to include lowercase characters");   
-  var upperCase = confirm("Click ok to include uppercase characters");    
-  var numericValue = confirm("Click ok to include numeric characters");   
-  var specialChars = confirm("Click ok to include special characters");
-  };
-
-  // objects
-  var responses = {
-    lowerCase: lowerCase,
-    upperCase: upperCase,
-    numericValue: numericValue,
-    specialChars: specialChars,
-    passwordLength: passwordLength
-  };
-    return responses;
-}
-
-// create strong responses from results
-function generatePassword() {
-  var passwordSelections = questions();
+  // create strong responses from results
+  var passwordSelections = askQuestions();
   var possibleOutcomes = [];
   var getPassword = "";
-  
-  if(passwordSelections.lowerCase) {
-    for(var i of lowerCase)
-    possibleOutcomes.push(i);
-  }
 
-  if(passwordSelections.upperCase) {
-    for(var i of upperCase)
-    possibleOutcomes.push(i);
-  }
-  if(passwordSelections.numericValue) {
-    for(var i of numericValue)
-    possibleOutcomes.push(i);
-  }
-  if(passwordSelections.specialChars) {
-    for(var i of specialChars)
-    possibleOutcomes.push(i);
-  }
-  
-  for (var i = 0; i < passwordSelections.passwordLength; i++) {
-    getPassword += possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.passwordLength)];
-  }
-    return getPassword;
-};
+function generatePassword() {
 
+    if(passwordSelections.confirmLowerCase) {
+      for (var i of lowerCase)
+        possibleOutcomes.push(i);
+    }
+
+    if(passwordSelections.confirmUpperCase) {
+      for (var i of upperCase)
+        possibleOutcomes.push(i);
+    }
+
+    if(passwordSelections.confirmNumericValue) {
+      for (var i of numericValue)
+        possibleOutcomes.push(i);
+    }
+
+    if(passwordSelections.confirmSpecialChars) {
+      for (var i of specialChars)
+        possibleOutcomes.push(i);
+    }
+
+    for (var i = 0; i < passwordSelections.passwordLength; i++) {
+        getPassword += possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
+      }
+      return getPassword;
+
+}
 
 // Write password to the #password input
 function writePassword() {
